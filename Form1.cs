cdbc423baef88c2fcad21895ef01d966f151b6f7
rodri <rodri@FAMILIAMEDINA>
2025-10-01 20:56:57 -0600

    cambio.003

--- Form1.cs
+++ Form1.cs
@@ -135,7 +135,7 @@ namespace _3OLIDTS_RodrigoNandayapa_04
         private bool EsTextoValido(string texto)
         {
 
-            return Regex.IsMatch(texto, @"^{[A-Za-z\\s]+$");
+            return Regex.IsMatch(texto, @"^[A-Za-z\s]+$");
         }
 
         private void label1_Click(object sender, EventArgs e)
